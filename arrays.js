b9a3355499dea1c0e643e91990c398d656ab9d5d
Logan Leonardson <logan.leonardson@gmail.com>
2018-07-26 17:01:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars=["snickers","hundred grand","kitkat","skittles"];
 
-function addElementToBeginningOfArray (chocolateBars,"3 musketeers") {
+function 
+ addElementToBeginningOfArray (chocolateBars,"3 musketeers") {
   return ["3 musketeers",...chocolateBars]
 }
