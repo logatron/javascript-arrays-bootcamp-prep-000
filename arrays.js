52881b6c56ff071a39274068966a7bdd2facba70
Logan Leonardson <logan.leonardson@gmail.com>
2018-07-26 18:11:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,6 +31,5 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   return array
 }
 function removeElementFromEndOfArray(array) {
-  array.slice(0,array.length-1)
-  return array
+  return array.slice(-1)
 }
