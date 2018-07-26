d9ec525c0241ab02b6f3c5ec3ea2e013b8e011f5
Logan Leonardson <logan.leonardson@gmail.com>
2018-07-26 17:37:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,6 +15,6 @@ function destructivelyAddElementToEndOfArray(array,element) {
   array.push(element)
   return array
 }
-function accessElementInArray(array,index) {
-  return element[index]
+function accessElementInArray(array[0]) {
+  return array[0]
 }
